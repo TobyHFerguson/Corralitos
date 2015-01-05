2690afd673fafca62ad9ca518c035fca598c7d99
Toby Ferguson <toby.h.ferguson@gmail.com>
2015-01-05 11:32:46 -0800

    Release of a simple LVM based installation with UEK and ensuring minimal size

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,2 +1,3 @@
+echo "Number of packages installed is: $(rpm -qa | wc -l)"
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
+yum -y install gcc make gcc-c++ kernel-uek-devel-$(uname -r)
